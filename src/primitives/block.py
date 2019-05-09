f0d2fa14ef02c103e8e6de0cd20a419fe0d1d8dd
dunky11 <tim.v.kaenel@web.de>
2019-05-09 19:38:00 +0200

    Added ability to mine coins

--- src/primitives/block.py
+++ src/primitives/block.py
@@ -3,23 +3,50 @@ from Crypto.Hash import SHA256
 
 
 class Block:
-    def __init__(self, index, micUnix, data, prevHash=""):
+    def __init__(self, index, unix, data, prevHash=""):
         self.index = index
-        self.micUnix = micUnix
+        self.unix = unix
         self.data = data
         self.prevHash = prevHash
+        """
+        Nonce is used so when we mine a coin we always
+        get a new hash and dont end up in an infinite loop
+        """
+        self.nonce = 0
         self.hash = self.calculateHash()
 
     def toString(self):
         retStr = "Block:\n"
         retStr += f"-index: {self.index}\n"
-        retStr += f"-unix: {self.micUnix}\n"
+        retStr += f"-unix: {self.unix}\n"
         retStr += f"-data: {self.data}\n"
         retStr += f"-prevHash: {self.prevHash}\n"
         retStr += f"-hash: {self.hash}\n"
+        retStr += f"-nonce: {self.nonce}\n"
         return retStr
 
     def calculateHash(self):
-        ecodedStr = (str(self.index) + str(self.micUnix) +
-                     dumps(self.data) + self.prevHash).encode("UTF-8")
+        ecodedStr = (str(self.index) + str(self.unix) +
+                     dumps(self.data) + self.prevHash + str(self.nonce)).encode("UTF-8")
         return SHA256.new(ecodedStr).hexdigest()
+
+    """
+    As we need proof of work, to validate that the blockchain is
+    not a completely different, calculated by someone else, all
+    hashes must have difficulty '0's as the first characters
+    """
+
+    def mineBlock(self, difficulty):
+        zeros = ""
+        """
+        PyLint will set unused variable for i if we define it
+        without _ (means intentionally used)
+        """
+        for _ in range(0, difficulty):
+            zeros += "0"
+        while(self.hash[0:difficulty] != zeros):
+            self.hash = self.calculateHash()
+            self.nonce += 1
+
+        print("---- New block was mined ----\n")
+        print(self.toString())
--- src/primitives/blockchain.py
+++ src/primitives/blockchain.py
@@ -1,28 +1,48 @@
 from block import Block
 from datetime import datetime
+import time
 
 
 def createGenesisBlock():
-    microUnix = datetime.now().microsecond
-    return Block(0, microUnix, "Let's create our chain...", "Im definitely not a hash value")
+    unix = time.time()
+    return Block(0, unix, "Let's create our chain...", "Im definitely not a hash value")
 
 
 class Blockchain:
-    def __init__(self):
+    def __init__(self, difficulty):
         self.chain = [createGenesisBlock()]
+        self.difficulty = difficulty
 
     def getLatestBlock(self):
         return self.chain[-1]
 
     def addBlock(self, block):
+        block.prevHash = self.getLatestBlock().hash
+        block.mineBlock(self.difficulty)
         self.chain.append(block)
 
+    def isValidChain(self):
+        # We skip the genesis block
+        for i in range(1, len(self.chain)):
+            curBlock = self.chain[i]
+            prevBlock = self.chain[i - 1]
+            if(curBlock.hash != curBlock.calculateHash()):
+                return False
+            if(curBlock.prevHash != prevBlock.hash):
+                return False
+        return True
+
     def toString(self):
-        retStr = "Blockchain:\n"
+        retStr = "Blockchain:\n\n"
         for block in self.chain:
-            retStr += block.toString()
+            retStr += f"{block.toString()}\n"
         return retStr
 
 
-blockchain = Blockchain()
-print(blockchain.toString())
+blockchain = Blockchain(4)
+blockchain.addBlock(Block(1, time.time(), {"coins": 10}))
+blockchain.addBlock(Block(2, time.time(), {"coins": 123}))
+blockchain.addBlock(Block(3, time.time(), {"coins": 110}))
+blockchain.addBlock(Block(4, time.time(), {"coins": 231}))
+blockchain.addBlock(Block(5, time.time(), {"coins": 123}))
+print(blockchain.isValidChain())
