1124db5fec4504d8cd73939570238606b6352db3
dunky11 <tim.v.kaenel@web.de>
2019-05-10 01:50:44 +0200

    Added react

---
+++ src/blockchain/main.js
@@ -0,0 +1,38 @@
+const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+// Your private key goes here
+const myKey = ec.keyFromPrivate('7c4c45907dec40c91bab3480c39032e90049f1a44f3e18c3e07c23e3273995cf');
+
+// From that we can calculate your public key (which doubles as your wallet address)
+const myWalletAddress = myKey.getPublic('hex');
+
+// Create new instance of Blockchain class
+const savjeeCoin = new Blockchain();
+
+// Create a transaction & sign it with your key
+const tx1 = new Transaction(myWalletAddress, 'address2', 100);
+tx1.signTransaction(myKey);
+savjeeCoin.addTransaction(tx1);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx2 = new Transaction(myWalletAddress, 'address1', 50);
+tx2.signTransaction(myKey);
+savjeeCoin.addTransaction(tx2);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+console.log();
+console.log(`Balance of xavier is ${savjeeCoin.getBalanceOfAddress(myWalletAddress)}`);
+
+// Uncomment this line if you want to test tampering with the chain
+// savjeeCoin.chain[1].transactions[0].amount = 10;
+
+// Check if the chain is valid
+console.log();
+console.log('Blockchain valid?', savjeeCoin.isChainValid() ? 'Yes' : 'No');
--- src/encrypt-utils/signMessage.py
+++
@@ -1,9 +0,0 @@
-from Crypto.Signature import pkcs1_15
-from Crypto.Hash import SHA256
-from Crypto.PublicKey import RSA
-
-
-def signMessage(message, secretKey):
-    key = RSA.import_key(secretKey)
-    hashed = SHA256.new(message)
-    return pkcs1_15.new(key).sign(hashed)
