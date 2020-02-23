9590d6213646abc64692be3d13ad401b0b6ae6f5
dunky11 <tim.v.kaenel@web.de>
2020-02-23 15:58:10 +0100

    Kept only compiled files

--- src/timchain/generateKeyPair.js
+++
@@ -1,9 +0,0 @@
-var EC = require("elliptic").ec;
-
-function generateKeyPair() {
-  const ec = new EC("secp256k1");
-  const key = ec.genKeyPair();
-  return { pk: key.getPublic("hex"), sk: key.getPrivate("hex") };
-}
-
-export default generateKeyPair;
