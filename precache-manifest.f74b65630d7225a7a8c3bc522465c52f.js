0c8373fdab9bf1ded6a4405368532e7023067643
dunky11 <tim.v.kaenel@web.de>
2020-03-01 22:58:09 +0100

    New version

---
+++ precache-manifest.f74b65630d7225a7a8c3bc522465c52f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b115e0387fa6d1e0a944b89f5ca7b3",
+    "url": "/centralia-coin/index.html"
+  },
+  {
+    "revision": "7339f8b52cff6546b7c6",
+    "url": "/centralia-coin/static/js/2.b84323a9.chunk.js"
+  },
+  {
+    "revision": "3a75fa42535f82ecbc939caa25e1f4a0",
+    "url": "/centralia-coin/static/js/2.b84323a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b749f74a9ce9bda7d2",
+    "url": "/centralia-coin/static/js/main.ebfc33ad.chunk.js"
+  },
+  {
+    "revision": "f269e4a4a5c80992e947",
+    "url": "/centralia-coin/static/js/runtime-main.81a3dfaa.js"
+  }
+]);
